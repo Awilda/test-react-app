c5e4eb8b918fc4478e7ffdbaa7910fd398589b3f
Awilda I Couverthier <awildacouverthier@gmail.com>
2020-03-15 18:21:31 -0400

    add files and folders

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestReactApp from './components/TestReactApp';
+
+ReactDOM.render(<TestReactApp />, document.getElementById('app'));
---
+++ src/components/TestReactApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class TestReactApp extends React.Component {
+  render() {
+    return (
+      <h1>Finally works!</h1>
+    );
+  }
+}
